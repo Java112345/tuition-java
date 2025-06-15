655ae1921a3b24c717d8317df424c3db9e90d684
Java112345 <javaproject112345@gmail.com>
2025-06-15 13:54:18 +0530

    refactor: move test classes to align with main package structure

--- src/main/java/com/ebtuition/model/Student.java
+++ src/main/java/com/ebtuition/model/Student.java
@@ -22,4 +22,21 @@ public class Student {
         this.address = address;
         this.emergencyContact = emergencyContact;
     }
+
+
+    public Student(String string, String string2, String string3, String string4, String string5) {
+        //TODO Auto-generated constructor stub
+    }
+
+
+    public Object getEmergencyContact() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getEmergencyContact'");
+    }
+
+
+    public Object getName() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getName'");
+    }
 }
