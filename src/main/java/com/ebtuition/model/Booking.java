6fa0efaad7c81b74f5f03d22ab5c97df43f0f2dc
Java112345 <javaproject112345@gmail.com>
2025-06-15 12:38:56 +0530

    Add attend() and addReview() methods to Booking model

--- src/main/java/com/ebtuition/model/Booking.java
+++ src/main/java/com/ebtuition/model/Booking.java
@@ -1,11 +1,36 @@
 package main.java.com.ebtuition.model;
 
 public class Booking {
-    private Student student;
-    private Lesson lesson;
-    private String status = "booked";
+    public Student student;
+    public Lesson lesson;
+    public String status = "booked";
     private String review;
     private int rating;
 
-    // Methods: attend(), addReview()
+    public Booking(Student student2, Lesson lesson2) {
+        this.student = student2;
+        this.lesson = lesson2;
+    }
+
+    public void attend() {
+        if ("booked".equals(this.status)) {
+            this.status = "attended";
+        }
+    }
+
+    public void addReview(String review, int rating) {
+        if ("attended".equals(this.status)) {
+            this.review = review;
+            this.rating = rating;
+        }
+    }
+
+    // Getters (optional) if you need to access private fields
+    public String getReview() {
+        return review;
+    }
+
+    public int getRating() {
+        return rating;
+    }
 }
